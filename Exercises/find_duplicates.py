d1d65d07b21fa9e233db87b36114bd28bf25a6e9
mehlam2805 <68456034+mehlam2805@users.noreply.github.com>
2020-07-26 19:12:36 +0530

    Update find_duplicates.py

--- Exercises/find_duplicates.py
+++ Exercises/find_duplicates.py
@@ -1,10 +1,12 @@
-# Exercise: Check for duplicates in list:
+# Exercise: Check for duplicates in list with the help of a function:
 some_list = ["a", "b", "c", "b", "d", "m", "n", "n"]
 
+def duplicate_finder(list)
 duplicates = []
-for item in some_list:
-    if some_list.count(item) > 1:
-        if item not in duplicates:
-            duplicates.append(item)
+    for item in list:
+        if list.count(item) > 1:
+            if item not in duplicates:
+                duplicates.append(item)
+    return duplicates       
 
-print(duplicates)
+print(duplicate_finder(some_list))
